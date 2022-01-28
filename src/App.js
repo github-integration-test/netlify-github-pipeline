a527216595a2b8e8c51c91bf9cfd8d704a36cd8c
Vladyslav Bihun <vladossuper363@gmail.com>
2022-01-28 19:04:03 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn FORGE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!          Learn FORGE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+          Learn FORGE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!          Learn FORGE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
       </header>
     </div>
   );
